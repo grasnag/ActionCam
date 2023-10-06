ddd0cf23d4f4f1d2ad54d94f91ffd3b3a89c6383
grasnag <Grasnag>
2023-10-06 17:51:41 +0300

    FSM working

--- camera.py
+++ camera.py
@@ -1,58 +1,71 @@
-from random import randint
-from time import Clock
+from gpiozero import LED, Button
+from signal import pause
+import time
 
+ledR = LED(17)
+ledG = LED(27)
+ledB = LED(22)
+btn = Button(23)
 
 ##============================================================================
 
 State = type("state", (object,),{})
 
+"""
 class State(object):
-    def__init__(self,FSM):
+    def __init__(self,FSM):
         self.FSM = FSM
 
-        def Enter(self:)
+        def Enter(self):
             pass
         def Execute(self):
             pass
         def Exit(self):
             pass
-        
+"""    
+
 class RecOn(State):
-    def__inint__(self,FSM):
-        super(RecOn, self).__init__(FSM)
-
-    def Enter(self):
-        print("Camera enter")
     def Execute(self):
-        print("Camera Execute")
-    def Exit(self):
-        print("Camera exit")
+        print("Recording")
+        ledR.on()
+        
+    #def __inint__(self,FSM):
+    #    super(RecOn, self).FSM
+    #def Enter(self):
+    #    print("Camera enter")
+    #def Exit(self):
+    #    print("Camera exit")
 
-class RecOff(state):
+class RecOff(State):
     def Execute(self):
-        print("Camera stop recording")
+        print("StopRecording")
+        ledG.on()
 
-class PreviewOn(state):
+class PreviewOn(State):
     def Execute(self):
+        ledB.on()
         print("preview")
 
-class StandBy(state):
+class StandBy(State):
     def Execute(self):
-        print("preview")
+        print("standby")
 
 ##============================================================================
 
 class Transition(object):
-    def__init__(self, toState):
+    def __init__(self, toState):
         self.toState = toState
 
     def Execute(self):
         print("transitioning")
+        ledR.off()
+        ledG.off()
+        ledB.off()
 
 ##============================================================================
 
 class CameraFSM(object):
-    def__init__(self, char):
+    def __init__(self, char):
         self.char = char
         self.states = {}
         self.transitions = {}
@@ -69,37 +82,71 @@ class CameraFSM(object):
 
     def Execute(self):
         if(self.trans):
-            self.curState.Exit()
+            self.SetState(self.trans.toState)
+            #self.curState.Exit()
+            #self.trans.Execute()
+            #self.curState.Enter()
+
             self.trans.Execute()
             self.SetState(self.trans.toState)
-            self.curState.Enter()
             self.trans = None
-
-            #self.trans.Execute()
-            #self.SetState(self.trans.toState)
-            #self.trans = None
         self.curState.Execute()
 
 ##============================================================================
 
 class Char(object):
-    def__init__(self).
-    self.FSM = CameraFSM(self)
-    self.RecOn = True
+    def __init__(self):
+        self.FSM = CameraFSM(self)
+        self.RecOff = True
 
 ##============================================================================
 
-if__name__=="__main__":
+if __name__ == "__main__":
     camera = Char()
-    camera.FSM.states[Rec] = RecOn()
-    camera.FSM.states[Stop] = RecOff()
-    camera.FSM.states[Preview] = PreviewOn()
-    camera.FSM.states[Stndb] = StandBy()
-    camera.FSM.transitions[toRec] = Transition("Rec")
-    camera.FSM.transitions[tostop] = Transition("Stop")
-    camera.FSM.transitions[toPreview] = Transition("Preview")
-    camera.FSM.transitions[toStndb] = Transition("Stndb")
-
-    camera.FSM.SetState("rec")
+    
+    camera.FSM.states["Rec"] = RecOn()
+    camera.FSM.states["RecOff"] = RecOff()
+    camera.FSM.states["Preview"] = PreviewOn()
+    camera.FSM.states["Stndb"] = StandBy()
+    camera.FSM.transitions["toRec"] = Transition("Rec")
+    camera.FSM.transitions["toRecOff"] = Transition("RecOff")
+    camera.FSM.transitions["toPreview"] = Transition("Preview")
+    camera.FSM.transitions["toStndb"] = Transition("Stndb")
+    camera.FSM.SetState("Rec")
 
 ##============================================================================
+
+def singlePress():
+    if (camera.RecOff):
+        camera.FSM.Transition("toRecOff")
+        camera.RecOff = False
+    else:
+        camera.FSM.Transition("toRec")
+        camera.RecOff = True
+    camera.FSM.Execute()
+        
+    print("siglePress!")
+    camera.FSM.Transition("toRec")
+
+def longPress():
+    print("longPress")
+    camera.FSM.Transition("toPreview")
+    camera.FSM.Execute()
+
+def pressed(btn):
+    hold_time = 0.5
+    start_time=time.time()
+    diff=0
+
+    while btn.is_active and (diff < hold_time) :
+        now_time=time.time()
+        diff =- start_time+now_time
+
+    if diff < hold_time :
+        singlePress()
+    else:
+        longPress()
+
+btn.when_pressed = pressed
+
+pause()
